fff05ef3f77d333a867ccc5c51dea7f19281fd2b
Documenter.jl <documenter@juliadocs.github.io>
2022-10-27 10:50:46 +0000

    build based on 1ad83f1

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PkgCacheInspector","category":"page"},{"location":"#PkgCacheInspector","page":"Home","title":"PkgCacheInspector","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PkgCacheInspector.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PkgCacheInspector]","category":"page"}]
+}
