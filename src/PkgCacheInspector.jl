51adae4701110e71e4689f958966f9dc6dca26d7
Tim Holy <tim.holy@gmail.com>
2022-10-28 10:44:34 -0500

    Initial working implementation
    For a not-yet-merged #44527

--- src/PkgCacheInspector.jl
+++ src/PkgCacheInspector.jl
@@ -1,17 +1,73 @@
 module PkgCacheInspector
 
+export info_cachefile, PkgCacheSizes, PkgCacheInfo
+
 using Base: PkgId, require_lock, assert_havelock, isvalid_cache_header, parse_cache_header, isvalid_file_crc,
             _tryrequire_from_serialized, find_all_in_cache_path, locate_package, stale_cachefile
+using Core: SimpleVector
 
-function extract_from_serialized(path::String, depmods::Vector{Any})
-    sv = ccall(:jl_return_package_image_components_from_stream, Any, (Cstring, Any), path, depmods)
+struct PkgCacheSizes
+    sysdata::Int
+    isbitsdata::Int
+    symboldata::Int
+    tagslist::Int
+    reloclist::Int
+    gvarlist::Int
+    fptrlist::Int
+end
+
+function Base.show(io::IO, szs::PkgCacheSizes)
+    nd = 0
+    nf = length(fieldnames(typeof(szs)))
+    for i = 1:nf
+        nd = max(nd, ndigits(getfield(szs, i)))
+    end
+    displaynames = [
+        "system",
+        "isbits",
+        "symbols",
+        "tags",
+        "relocations",
+        "gvars",
+        "fptrs"
+    ]
+    l = maximum(length, displaynames)
+    for i = 1:nf
+        println(io, rpad(displaynames[i], l), ": ", lpad(string(getfield(szs, i)), nd))
+    end
+end
+
+struct PkgCacheInfo
+    modules::Vector{Any}
+    init_order::Vector{Any}
+    external_methods::Vector{Any}
+    new_specializations::Vector{Any}
+    new_method_roots::Vector{Any}
+    external_targets::Vector{Any}
+    edges::Vector{Any}
+    cachesizes::PkgCacheSizes
+end
+
+function Base.show(io::IO, info::PkgCacheInfo)
+    println(io, "modules: ", info.modules)
+    !isempty(info.init_order) && println(io, "init order: ", info.init_order)
+    !isempty(info.external_methods) && println(io, length(info.external_methods), " external methods")
+    !isempty(info.new_specializations) && println(io, length(info.new_specializations), " new specializations of external methods")
+    !isempty(info.new_method_roots) && println(io, length(info.new_method_roots) ÷ 2, " external methods with new roots")
+    !isempty(info.external_targets) && println(io, length(info.external_targets) ÷ 3, " external targets")
+    !isempty(info.edges) && println(io, length(info.edges) ÷ 2, " edges")
+    show(io, info.cachesizes)
+end
+
+function info_cachefile(path::String, depmods::Vector{Any})
+    sv = ccall(:jl_restore_incremental, Any, (Cstring, Any, Cint), path, depmods, true)
     if isa(sv, Exception)
         throw(sv)
     end
-    return sv::SimpleVector
+    return PkgCacheInfo(sv[1:7]..., PkgCacheSizes(sv[8]...))
 end
 
-function extract_from_serialized(pkg::PkgId, path::String)
+function info_cachefile(pkg::PkgId, path::String)
     return @lock require_lock begin
         local depmodnames
         io = open(path, "r")
@@ -33,13 +89,11 @@ function extract_from_serialized(pkg::PkgId, path::String)
             depmods[i] = dep
         end
         # then load the file
-        extract_from_serialized(path, depmods)
+        info_cachefile(path, depmods)
     end
 end
 
-# extract_from_serialized(pkg::PkgId, env=nothing) = extract_from_serialized(pkg, locate_package(pkg, env))
-
-function extract_from_serialized(pkg::PkgId)
+function info_cachefile(pkg::PkgId)
     cachefiles = find_all_in_cache_path(pkg)
     isempty(cachefiles) && error(pkg, " has not yet been precompiled for julia ", Base.VERSION)
     pkgpath = locate_package(pkg)
@@ -47,8 +101,9 @@ function extract_from_serialized(pkg::PkgId)
         stale_cachefile(pkgpath, cf) !== true
     end
     idx === nothing && error("all cache files for ", pkg, " are stale, please precompile")
-    return extract_from_serialized(pkg, cachefiles[idx])
+    return info_cachefile(pkg, cachefiles[idx])
 end
-extract_from_serialized(pkgname::AbstractString) = extract_from_serialized(Base.identify_package(pkgname))
+
+info_cachefile(pkgname::AbstractString) = info_cachefile(Base.identify_package(pkgname))
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,8 @@ using PkgCacheInspector
 using Test
 
 @testset "PkgCacheInspector.jl" begin
-    # Write your tests here.
+    info = info_cachefile("Colors")
+    @test isa(info, PkgCacheInfo)
+    str = sprint(show, info)
+    @test occursin("relocations", str) && occursin("new specializations", str) && occursin("targets", str)
 end
